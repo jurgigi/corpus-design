ad1ebf4ae28ff8685d8ccba1f50034896826d511
jurgigi <119338554+jurgigi@users.noreply.github.com>
2023-05-15 15:57:07 +0100

    Update HAL_theses_API.py

--- Corpus tools/HAL_theses_API.py
+++ Corpus tools/HAL_theses_API.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python3
 
 import requests
 
 QUERY = 'bioinformatique OR bioinformatics OR bio-informatique'
 
 # construct the API request URL
-#url = f'https://api.archives-ouvertes.fr/search/?q={QUERY}&fq=docType_s%3A%22TH%22&fl=halId_s&wt=json'
 url = f'https://api.archives-ouvertes.fr/search/tel/?q={QUERY}&rows=1000&wt=json'
 
 # send the API request
@@ -17,11 +15,11 @@ if response.status_code == 200:
 	data = response.json()
 	docs = data['response']['docs']
 	
-	with open('/Users/jurgigiraud/Documents/results.txt', 'w') as f:
+	with open('', 'w') as f:
 			for doc in docs:
 				f.write(str(doc) + '\n')
 				
-	print('Results saved into results.txt.')
+	print('Results saved.')
 
 	
 else:
